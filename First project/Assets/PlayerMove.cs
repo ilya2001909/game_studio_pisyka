61af9f8f604a02cdf86c89c3f054c10838c70c7a
ilya2001909 <devyatkovilya93@gmail.com>
2020-11-24 05:29:02 +0300

    add_animation

--- First project/Assets/PlayerMove.cs
+++ First project/Assets/PlayerMove.cs
@@ -3,42 +3,45 @@
 public class PlayerMove : MonoBehaviour
 {
     public CharacterController controller;
+    public Joystick joystick;
     public float speed;
     public float speed_pov;
-    private Vector3 velocity;
+    private float turnSmoothVelocity;
+    private Animator animator;
+    public Event events;
 
     void Start()
     {
         speed_pov /= 100;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x = joystick.Horizontal;
+        float z = joystick.Vertical;
+        Vector3 direction = new Vector3(x, 0f, z).normalized;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (direction.magnitude >= 0.1f)
         {
-            speed = speed * 2f;  
+            float targetAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref turnSmoothVelocity, speed_pov);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+            animator.SetBool("isAttacking", false);
+            animator.SetBool("isRunning", true);
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        else
         {
-            speed = speed / 2f;
+            animator.SetBool("isRunning", false);
         }
 
+    }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-        controller.Move(velocity * Time.deltaTime);
-        
-        
-        float y_forward = transform.forward.y;
-
-        Vector3 _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        Vector3 direct = Vector3.RotateTowards(transform.forward, _moveDirection, speed_pov, 0.0f);
-        transform.rotation = Quaternion.LookRotation(direct);
-
+    public void attack()
+    {
+        animator.SetBool("isAttacking", true);
     }
 }
