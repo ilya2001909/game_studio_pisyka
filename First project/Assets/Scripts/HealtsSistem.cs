684a55c829118ad562953af946d408f8d9e879f7
ilya2001909 <devyatkovilya93@gmail.com>
2020-11-25 23:43:41 +0300

    Complete_gen
    1 слияние работ

---
+++ First project/Assets/Scripts/HealtsSistem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealtsSistem : MonoBehaviour
+{
+    public int health;
+
+    public List<Image> helf_images = new List<Image>(6);
+
+    public Sprite fullLive;
+    public Sprite emptyLive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < helf_images.Count; i++)
+        {
+            if (i<health)
+            {
+                helf_images[i].sprite = fullLive;
+            }
+            else
+            {
+                helf_images[i].sprite = emptyLive;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
